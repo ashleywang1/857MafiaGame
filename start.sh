2255607968a2f8b816c93253e4be2b9f7c2f7792
Ashley Wang <ashwang@dhcp-18-189-63-57.dyn.MIT.EDU>
2016-04-16 15:37:57 -0400

    generalized start script

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-pkill -f 'python2 mafia.py'&
-#kill -9 $(ps -ea | grep mafia)&
+pkill -f '[Pp]ython.? mafia.py'&
 wait $!
 for i in {0..10}; do
   python2 mafia.py '8870, 8871, 8872, 8873, 8874, 8875, 8876, 8877, 8878, 8879, 8880' $i &
