16044fd6e4d15f96d79cf18906d547bf7a320cf3
Ronald Gil <rongil@mit.edu>
2016-04-16 17:20:58 -0400

    Even cleaner setup script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 pkill -f '[Pp]ython.? mafia.py'&
 wait $!
+
+ports=$(echo {8870..8880} | sed 's/ /,/g')
 for i in {0..10}; do
-  python3 mafia.py '8870, 8871, 8872, 8873, 8874, 8875, 8876, 8877, 8878, 8879, 8880' $i &
+  python3 mafia.py $ports $i &
 done
